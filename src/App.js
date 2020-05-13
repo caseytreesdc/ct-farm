785ecf04ec12b23e13d0094c4d102faa6afd2099
Tissa Khosla <tissakhosla@Tissas-MacBook-Pro.local>
2020-05-13 15:40:32 -0400

    pre-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./App.css";
 import "./components/Splash.css";
-import "./components/Video.css"
+import "./components/Video.css";
 import splashVideo from "./assets/WebHeroClip.mp4";
 
-function Main() {
+function Splash() {
   return (
     <div className="Splash">
       <div className="Video-box">
@@ -16,22 +16,25 @@ function Main() {
   );
 }
 
-function Gallery(){
-  let thumbsrc1="https://caseytrees.org/wp-content/uploads/2019/10/eoy-test-farm-video-article.jpg"
-  let thumbsrc2="https://caseytrees.org/wp-content/uploads/2019/09/farm-ctp-19-article.jpg"
-  let thumbsrc3="https://caseytrees.org/wp-content/uploads/2018/07/NightFarming-article.jpg"
+function Gallery() {
+  let thumbsrc1 =
+    "https://caseytrees.org/wp-content/uploads/2019/10/eoy-test-farm-video-article.jpg";
+  let thumbsrc2 =
+    "https://caseytrees.org/wp-content/uploads/2019/09/farm-ctp-19-article.jpg";
+  let thumbsrc3 =
+    "https://caseytrees.org/wp-content/uploads/2018/07/NightFarming-article.jpg";
   return (
     <div className="Gallery">
-      <img className="Thumbnail" src={thumbsrc1}></img>
-      <img className="Thumbnail" src={thumbsrc2}></img>
-      <img className="Thumbnail" src={thumbsrc3}></img>
+      <img className="Gallery__thumbnail" src={thumbsrc1}></img>
+      <img className="Gallery__thumbnail" src={thumbsrc2}></img>
+      <img className="Gallery__thumbnail" src={thumbsrc3}></img>
     </div>
-  )
+  );
 }
 function App() {
   return (
     <div className="App">
-      <Main></Main>
+      <Splash></Splash>
       <Gallery></Gallery>
     </div>
   );
