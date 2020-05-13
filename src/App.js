f9ac1d5ce1ede3b9df3a62d3b837f4be85c5b1f2
Tissa Khosla <tissakhosla@Tissas-MacBook-Pro.local>
2020-05-13 15:37:02 -0400

    video/thumbnail sizing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import React from "react";
 import "./App.css";
-
-import heroVideo from "./assets/WebHeroClip.mp4"; 
+import "./components/Splash.css";
+import "./components/Video.css"
+import splashVideo from "./assets/WebHeroClip.mp4";
 
 function Main() {
   return (
-    <div className="Hero">
-      <video loop autoPlay>
-        <source src={heroVideo} type="video/mp4"></source>
-      </video>
+    <div className="Splash">
+      <div className="Video-box">
+        <video className="Video" loop autoPlay>
+          <source src={splashVideo} type="video/mp4"></source>
+        </video>
+      </div>
     </div>
   );
 }
 
+function Gallery(){
+  let thumbsrc1="https://caseytrees.org/wp-content/uploads/2019/10/eoy-test-farm-video-article.jpg"
+  let thumbsrc2="https://caseytrees.org/wp-content/uploads/2019/09/farm-ctp-19-article.jpg"
+  let thumbsrc3="https://caseytrees.org/wp-content/uploads/2018/07/NightFarming-article.jpg"
+  return (
+    <div className="Gallery">
+      <img className="Thumbnail" src={thumbsrc1}></img>
+      <img className="Thumbnail" src={thumbsrc2}></img>
+      <img className="Thumbnail" src={thumbsrc3}></img>
+    </div>
+  )
+}
 function App() {
   return (
     <div className="App">
       <Main></Main>
+      <Gallery></Gallery>
     </div>
   );
 }
