bb7864f40b6bb640eee7c1ffffe629759bfd8135
Tissa Khosla <tissakhosla@Tissas-MacBook-Pro.local>
2020-05-13 15:04:12 -0400

    added video

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import heroVideo from "./assets/WebHeroClip.mp4"; 
+
+function Main() {
+  return (
+    <div className="Hero">
+      <video loop autoPlay>
+        <source src={heroVideo} type="video/mp4"></source>
+      </video>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Main></Main>
     </div>
   );
 }
