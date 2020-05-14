ef7d4cf845a9bb7d3edb31cd36f7e0548fd2324f
Tissa Khosla <tissakhosla@Tissas-MacBook-Pro.local>
2020-05-14 08:31:17 -0400

    pre deploy pre gh refrax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { BrowserRouter, Link, Switch, Route } from "react-router-dom";
 import "./App.css";
 import "./components/Splash.css";
 import "./components/Video.css";
+import "./components/Gallery.css";
+import "./components/Page.css";
+
 import splashVideo from "./assets/WebHeroClip.mp4";
 
 function Splash() {
@@ -16,26 +20,68 @@ function Splash() {
   );
 }
 
+function About() {
+  return (
+    <div className="About Page">
+      <h1>This is the About and History page</h1>
+    </div>
+  );
+}
+
+function Trees() {
+  return (
+    <div className="Trees Page">
+      <h1>This is the Trees page</h1>
+    </div>
+  );
+}
+
+function Sustainability() {
+  return (
+    <div className="Sustainibility Page">
+      <h1>This is the Sustainability page</h1>
+    </div>
+  );
+}
+
 function Gallery() {
   let thumbsrc1 =
     "https://caseytrees.org/wp-content/uploads/2019/10/eoy-test-farm-video-article.jpg";
   let thumbsrc2 =
     "https://caseytrees.org/wp-content/uploads/2019/09/farm-ctp-19-article.jpg";
   let thumbsrc3 =
+    "https://caseytrees.org/wp-content/uploads/2017/07/Farm-article-A.jpg";
+  let thumbsrc4 =
     "https://caseytrees.org/wp-content/uploads/2018/07/NightFarming-article.jpg";
   return (
     <div className="Gallery">
-      <img className="Gallery__thumbnail" src={thumbsrc1}></img>
-      <img className="Gallery__thumbnail" src={thumbsrc2}></img>
-      <img className="Gallery__thumbnail" src={thumbsrc3}></img>
+      <Link to="/">
+        <img className="Gallery__thumbnail" src={thumbsrc1}></img>
+      </Link>
+      <Link to="/About">
+        <img className="Gallery__thumbnail" src={thumbsrc2}></img>
+      </Link>
+      <Link to="/Trees">
+        <img className="Gallery__thumbnail" src={thumbsrc3}></img>
+      </Link>
+      <Link to="/Sustainability">
+        <img className="Gallery__thumbnail" src={thumbsrc4}></img>
+      </Link>
     </div>
   );
 }
 function App() {
   return (
     <div className="App">
-      <Splash></Splash>
-      <Gallery></Gallery>
+      <BrowserRouter>
+        <Switch>
+          <Route component={Splash} exact path="/"></Route>
+          <Route component={About} path="/About"></Route>
+          <Route component={Trees} path="/Trees"></Route>
+          <Route component={Sustainability} path="/Sustainability"></Route>
+        </Switch>
+        <Gallery></Gallery>
+      </BrowserRouter>
     </div>
   );
 }
