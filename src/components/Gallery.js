41431db00dcfdeaa65394bef39e62b30633a9230
Tissa Khosla <tissakhosla@Tissas-MacBook-Pro.local>
2020-05-14 13:52:44 -0400

    added captions

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -8,23 +8,27 @@ import sustainability_thumbnail from "../assets/thumbnails/sustainability.jpg";
 
 import "./Gallery.css";
 
-function Gallery() {
+function Gallery(props) {
   return (
     <div className="Gallery">
       <Link to="/">
         <img className="Gallery__thumbnail" src={home_thumbnail}></img>
+        <h2 className="Gallery__caption">Home</h2>
       </Link>
       <Link to="/About">
         <img className="Gallery__thumbnail" src={about_thumbnail}></img>
+        <h2 className="Gallery__caption">About</h2>
       </Link>
       <Link to="/Trees">
         <img className="Gallery__thumbnail" src={trees_thumbnail}></img>
+        <h2 className="Gallery__caption">Trees</h2>
       </Link>
       <Link to="/Sustainability">
         <img
           className="Gallery__thumbnail"
           src={sustainability_thumbnail}
         ></img>
+        <h2 className="Gallery__caption">Sustainability</h2>
       </Link>
     </div>
   );
