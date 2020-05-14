aca083ad47fbdf107dc86c768dc81b2a3c7f6c7e
Tissa Khosla <tissakhosla@Tissas-MacBook-Pro.local>
2020-05-14 08:39:30 -0400

    Updates

---
+++ precache-manifest.5e1229e70293e4a401656568e4fdbd3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ffaa74f3fae3bc27d9512047fdc422",
+    "url": "/ct-videos/index.html"
+  },
+  {
+    "revision": "e8e08afc99773feb99c0",
+    "url": "/ct-videos/static/css/main.031b808d.chunk.css"
+  },
+  {
+    "revision": "25e5ce7fed13db8b44e2",
+    "url": "/ct-videos/static/js/2.04fa2ec3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ct-videos/static/js/2.04fa2ec3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8e08afc99773feb99c0",
+    "url": "/ct-videos/static/js/main.676eb31f.chunk.js"
+  },
+  {
+    "revision": "e034e0db555eb2276c19",
+    "url": "/ct-videos/static/js/runtime-main.9e5c72dc.js"
+  },
+  {
+    "revision": "9f0ce16390e50338f05b651aa347046b",
+    "url": "/ct-videos/static/media/WebHeroClip.9f0ce163.mp4"
+  }
+]);
