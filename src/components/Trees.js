69acbfd486d82613b10f0933f8bd4c08c8d6fe75
Tissa Khosla <tissakhosla@Tissas-MacBook-Pro.local>
2020-05-19 19:30:57 -0400

    normal checkbox

--- src/components/Trees.js
+++ src/components/Trees.js
@@ -1,12 +1,28 @@
 import React from "react";
-
+import "./Checkbox.css";
 class Trees extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {};
+    this.handleClick = this.handleClick.bind(this);
+    this.selection = this.selection.bind(this);
   }
+
+  handleClick() {
+    let checkBox = document.getElementsByClassName("Checkbox");
+    console.log(checkBox[0].checked);
+  }
+
   render() {
-    return <div>Trees</div>;
+    return (
+      <form>
+        <input
+          onClick={this.handleClick}
+          className="Checkbox"
+          type="checkbox"
+        ></input>
+      </form>
+    );
   }
 }
-
 export default Trees;
