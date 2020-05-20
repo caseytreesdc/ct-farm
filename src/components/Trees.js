b52b813514e8baced5660197a18dc116d1adee85
Tissa Khosla <tissakhosla@Tissas-MacBook-Pro.local>
2020-05-20 11:04:53 -0400

    checkboxes

--- src/components/Trees.js
+++ src/components/Trees.js
@@ -1,27 +1,91 @@
 import React from "react";
-import "./Checkbox.css";
+import json from "../assets/speciesList.json";
+import "./Species.css"
+// import "./Checkbox.css";
 class Trees extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      trees: json,
+      display: [],
+    };
     this.handleClick = this.handleClick.bind(this);
     this.selection = this.selection.bind(this);
   }
 
+  selection() {
+    let checkBoxCollection = document.getElementsByClassName("Checkbox");
+    let displayList = [];
+    for (let i = 0; i < checkBoxCollection.length; i++) {
+      if (checkBoxCollection[i].checked) {
+        let selectedProperty = checkBoxCollection[i].id;
+        console.log(selectedProperty);
+        this.state.trees.forEach((element) => {
+          if (element.size === selectedProperty) {
+            displayList.push(
+              <div className="Species__card">
+                <img
+                  className="Species__sketch"
+                  alt={element.common}
+                  src={process.env.PUBLIC_URL + "/img/" + element.sketch}
+                ></img>
+                <div>
+                  <h3>
+                    <span className="Species__latin">{element.latin} | </span>
+                    <span className="Species__common">{element.common}</span>
+                  </h3>
+                  <p className="Species__info"></p>
+                  <p className="Species__info">ZONE: {element.zone}</p>
+                  <p className="Species__info">
+                    MATURE HEIGHT, SPREAD: {element.height}', {element.spread}'
+                  </p>
+                  <p className="Species__info">NOTES: {element.notes}</p>
+                </div>
+              </div>
+            );
+          }
+        });
+      }
+    }
+    this.setState({ display: displayList });
+    console.dir(this.state.display);
+  }
+
   handleClick() {
-    let checkBox = document.getElementsByClassName("Checkbox");
-    console.log(checkBox[0].checked);
+    this.selection();
   }
 
   render() {
     return (
-      <form>
-        <input
-          onClick={this.handleClick}
-          className="Checkbox"
-          type="checkbox"
-        ></input>
-      </form>
+      <div>
+        <div>
+          <form className="">
+            <p>Size</p>
+            <input
+              onClick={this.handleClick}
+              className="Checkbox"
+              type="checkbox"
+              id="Small"
+            ></input>
+            <label for="Small">Small</label>
+            <input
+              onClick={this.handleClick}
+              className="Checkbox"
+              type="checkbox"
+              id="Medium"
+            ></input>
+            <label for="Medium">Medium</label>
+            <input
+              onClick={this.handleClick}
+              className="Checkbox"
+              type="checkbox"
+              id="Large"
+            ></input>
+            <label for="Large">Large</label>
+          </form>
+        </div>
+        <div>{this.state.display}</div>
+      </div>
     );
   }
 }
